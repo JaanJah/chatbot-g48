3cd3fc1b784a9e1ac83908aebf48ae10d4a494fc
jaanjahimees <jaanjahilo@outlook.com>
2018-10-20 09:35:38 +0300

    dataservice and core file.

---
+++ chatbot/chatbot/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace chatbot
+{
+    class DataService
+    {
+
+        public static async Task<dynamic> HandleAsyncRequest(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            dynamic data = null;
+
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+
+            return data;
+
+        }
+    }
+}
