247bc2cde42bb6012e0997688bca138394c9018e
Roberts Pauls Tohver <robertspauls.tohver(at)tptlive.ee>
2018-10-20 12:16:45 +0300

    initial commit of a basic UI view

--- chatbot/chatbot/MainActivity.cs
+++ chatbot/chatbot/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace chatbot
 {
@@ -14,6 +15,8 @@ namespace chatbot
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //var chatField = FindViewById<EditText>();
         }
     }
 }
