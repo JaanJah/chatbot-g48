fe872379a359ad51db28d885956798436de7bcde
Roberts Pauls Tohver <robertspauls.tohver(at)tptlive.ee>
2018-10-21 14:47:31 +0300

    just another push

--- chatbot/chatbot/ChatAdapter.cs
+++ chatbot/chatbot/ChatAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using static Android.App.ActionBar;
 
 namespace chatbot
 {
@@ -43,18 +45,21 @@ namespace chatbot
             View view = convertView; // re-use an existing view, if one is available
             if (view == null) // otherwise create a new one
                 view = context.LayoutInflater.Inflate(Resource.Layout.CustomAdapter, null);
-            view.FindViewById<TextView>(Resource.Id.textView1).Text = sent[position];
-            view.FindViewById<TextView>(Resource.Id.botMessage).Text = received[position];
+            view.FindViewById<TextView>(Resource.Id.textView1).Text = sent[position] + "  ";
+            view.FindViewById<TextView>(Resource.Id.botMessage).Text = "  " + received[position];
+            
             if (sender[position] == true)
             {
-                view.FindViewById<TextView>(Resource.Id.textView1).SetBackgroundColor(Android.Graphics.Color.White);
-                view.FindViewById<TextView>(Resource.Id.botMessage).SetBackgroundColor(Android.Graphics.Color.White);
+                view.FindViewById<TextView>(Resource.Id.textView1).SetBackgroundColor(Android.Graphics.Color.GhostWhite);
+                //view.FindViewById<TextView>(Resource.Id.botMessage).SetBackgroundColor(Android.Graphics.Color.GhostWhite);
+                //view.FindViewById<TextView>(Resource.Id.textView1).LayoutParameters = view.LayoutParameters.
             }
             if (sender[position] == false)
             {
-                view.FindViewById<TextView>(Resource.Id.textView1).SetBackgroundColor(Android.Graphics.Color.BlueViolet);
-                view.FindViewById<TextView>(Resource.Id.botMessage).SetBackgroundColor(Android.Graphics.Color.BlueViolet);
+                //view.FindViewById<TextView>(Resource.Id.textView1).SetBackgroundColor(Android.Graphics.Color.LightBlue);
+                view.FindViewById<TextView>(Resource.Id.botMessage).SetBackgroundColor(Android.Graphics.Color.LightBlue);
             }
+            //view.FindViewById<ImageButton>(Resource.Id.sendInputBtn).SetBackgroundColor(Android.Graphics.Color.Transparent);
             return view;
         }
     }
